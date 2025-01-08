0219dae3f9088e61783589159020f7de09bca06b
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-08 12:48:51 +0100

    feat: adapted for react

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const menu = require("./data");
 const pizzasRouter = require("./routers/pizzas");
+const ingredientsRouter = require("./routers/ingredients");
 const handleError = require("./middlewares/handleError");
+const notFoundRoute = require("./middlewares/notFoundRoute");
+const cors = require("cors");
+
+// npm install cors
+const corsMiddleware = require("cors");
 
 const app = express();
 const port = 3001;
+
+// Diamo il permesso a questo indirizzo di richiedere i dati
+// app.use(corsMiddleware({
+//   origin: 'http://127.0.0.1:5500'
+// }));
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173'
+}));
 
 // aggiungo il body parser in formato json per poter leggere il body della richiesta quando arriva alle rotte post, put e patch
 app.use(express.json());
@@ -12,8 +28,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
 // includo tutte le rotte delle pizze con prefisso "pizzas" nelle url di ogni rotta
 app.use("/pizzas", pizzasRouter);
+app.use("/ingredients", ingredientsRouter);
 
 app.get("/", (req, res) => {
   // pippo.ciao();
@@ -35,17 +53,11 @@ app.get('/ricerca', (req, res) => {
   res.json(pizzas);
 });
 
+app.use(notFoundRoute);
+
 // Dopo tutte le rotte inseriamo il moddleware che gestisce errore
 app.use(handleError);
 
 app.listen(port, () => {
   console.log("Server is listening");
 });
-
-
-
-
-
-
-
-
